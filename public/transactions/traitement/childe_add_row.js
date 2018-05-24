553ba68a28249fdc995f7e5d979be0941392601c
wassim hazime <wassim.hazime@gmail.com>
2018-05-24 02:57:50 +0000

    java script file

---
+++ public/transactions/traitement/childe_add_row.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    var content_child = $("#content-child");
+    var inputs_child = $(".inputs-child");
+    var add_button = $("#add_row");
+    var idfile=1;
+    $(add_button).click(function(e) {
+        e.preventDefault();
+      
+       
+        var new_row = inputs_child.clone();
+        //style file
+          idfile++;
+        var id_file = 'idfile' + idfile;
+       new_row.find("input[type='file']").attr("id", id_file);
+        new_row.find("label").attr("for", id_file);
+        new_row.find("label span").text("");
+        new_row.find("input").val("");
+        new_row.find("textarea").val("");
+        
+        $(content_child).append(new_row); //add input box
+
+
+        var inputs = document.querySelectorAll('.inputfile');
+
+
+
+
+        Array.prototype.forEach.call(inputs, function(input)
+        {
+            var label = input.previousElementSibling;
+            input.addEventListener('change', function(e)
+            {
+                label.querySelector('span').innerHTML = this.files.length;
+            }
+            );
+        });
+
+    });
+
+    $(content_child).on("click", ".delete", function(e) {
+        e.preventDefault();
+        $(this).parent('td').parent('tr').remove();
+
+    });
+});
+
---
+++ public/transactions/traitement/stylefile.js
@@ -0,0 +1,26 @@
+/// class .inputfile
+var inputs = document.querySelectorAll('.inputfile');
+var id_math="id_math"+Math.ceil(Math.random() * 1000);
+ var id_file = 'id_file' + id_math;
+ $(inputs).attr("id",id_file);
+$('  <label for="' + id_file + '"  ><span class="glyphicon glyphicon-paperclip btn btn-xs " style="font-size: 16px ;    background-color: #dcecfb;" ></span></label>').insertBefore(inputs);
+$(inputs).css({
+    "width": "0.1px",
+    "height": "0.1px",
+    "opacity": "0",
+    "overflow": "hidden",
+    "position": "absolute",
+    " z-index": "-1"
+})
+
+
+
+Array.prototype.forEach.call(inputs, function(input)
+{
+    var label = input.previousElementSibling;
+    input.addEventListener('change', function(e)
+    {
+        label.querySelector('span').innerHTML = this.files.length;
+    }
+    );
+});
