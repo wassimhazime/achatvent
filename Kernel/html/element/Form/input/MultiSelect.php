7f2c8c3a52bcc44bca05b6bdc0926d51914a7cd0
wassim hazime <wassim.hazime@gmail.com>
2018-05-31 00:50:40 +0000

    code multiselect

--- Kernel/html/element/Form/input/MultiSelect.php
+++ Kernel/html/element/Form/input/MultiSelect.php
@@ -21,19 +21,18 @@ class MultiSelect extends Abstract_Input {
 
     //put your code here
 
-    private function data_load() {
-        $data_load = [];
-        foreach ($this->input['Data_load'] as $row) {
+    private function value_att($data) {
+        $data_value_att = [];
+        foreach ($data as $row) {
             $ligne = "";
             $att_data_html = "";
             foreach ($row as $column => $value) {
                 $ligne .= $column . '$$$' . $value . ' £££ ';
                 $att_data_html .= " data-content_" . $column . '="' . $value . '" ';
             }
-        
-            $data_load[$row->id] = ["value" => $ligne, "att" => $att_data_html];
+            $data_value_att[$row->id] = ["value" => $ligne, "att" => $att_data_html];
         }
-        return $data_load;
+        return $data_value_att;
     }
 
     private function chargeOption($data_load, $Default) {
@@ -44,46 +43,40 @@ class MultiSelect extends Abstract_Input {
                             ->setAtt($data["att"])
                             ->setData($data["value"])->builder();
         }
-
-        foreach ($Default as $row) {
-            $dataOption = "";
-            foreach ($row as $column => $value) {
-                $dataOption .= $column . '$$$' . $value . ' £££ ';
-            }
+        foreach ($Default as $id => $data) {
             $optionTag [] = HTML::TAG('option')
-                            ->setValue($row->id)
-                            ->setAtt(' data-container="body" '
-                                    . 'data-toggle="popover"'
-                                    . ' data-placement="top"'
-                                    . ' data-content="' . $dataOption . ' " selected ')
-                            ->setData($dataOption)->builder();
+                            ->setValue($id)
+                            ->setAtt($data["att"] . '  selected ')
+                            ->setData($data["value"])->builder();
         }
+
         return $optionTag;
     }
 
     public function builder() {
         $name = $this->name;
         $id_html = $this->id_html;
-        $Default = $this->Default;
-        $data_load = $this->data_load();
+
+        $Default = $this->value_att($this->Default);
+        $data_load = $this->value_att($this->input['Data_load']);
 
 //////////////////////////////////////////////////////
         $optionTag = $this->chargeOption($data_load, $Default);
         $Multiselecttag = HTML::TAG("select")
-                ->setAtt('multiple ')
-                ->setAtt('  data-set_null="'.$this->null.'" ')
+                ->setAtt(' multiple ')
+                ->setAtt('  data-set_null="' . $this->null . '" ')
                 ->setClass("  form-control ")
-                 ->setId($id_html)
+                ->setId($id_html)
                 ->setName($name . '[]')
                 ->setData($optionTag)
                 ->builder();
 
-        return $this->div($Multiselecttag,count($optionTag));
+        return $this->div($Multiselecttag, count($optionTag));
     }
 
-    protected function div($Multiselecttag,$badge="") {
-        
-        $modal = new Modals($this->lable, $Multiselecttag,"",$badge);
+    protected function div($Multiselecttag, $badge = "") {
+
+        $modal = new Modals($this->lable, $Multiselecttag, "", $badge);
 
         return $modal->builder();
     }
--- public/comptable/traitement/calculeTTC.js
+++ public/comptable/traitement/calculeTTC.js
@@ -1,55 +1,62 @@
-$(document).ready(function () {
-  function plus(a, b) {
-    return ((a + b).toFixed(2));
-  }
-  function moins(a, b) {
-    return ((a - b).toFixed(2));
-  }
+$(document).ready(function() {
+    /// class calcul
+    var Calcule = function(HT, TVA, TTC) {
+        this.HT = HT;
+        this.TVA = TVA;
+        this.TTC = TTC;
+    }
+    Calcule.prototype = {
+        plus: function(a, b) {
+            return ((a + b).toFixed(2));
+        },
+        moins: function(a, b) {
+            return ((a - b).toFixed(2));
+        },
+        ht: function(HT) {
+            var valueHT = ($(HT).val()) * 1;
+            var valueTVA = (valueHT * 0.2);
+            var valueTTC = this.plus(valueTVA, valueHT);
+            this.TVA.val(valueTVA.toFixed(2));
+            this.TTC.val(valueTTC);
+        },
+        tva: function(TVA) {
+            var valueHT = ($ht.val()) * 1;
+            var valueTVA = ($(TVA).val()) * 1;
+            var valueTTC = this.plus(valueTVA, valueHT);
+            this.TTC.val(valueTTC);
+        },
+        ttc: function(TTC) {
+            var valueTTC = ($(TTC).val()) * 1;
+            var valueHT = (valueTTC / 1.2);
+            var valueTVA = this.moins(valueTTC, valueHT);
+            this.TVA.val(valueTVA);
+            this.HT.val(valueHT.toFixed(2));
+        }
+
+    }
+    /////
+
+    if (($("#id_html_montant_HT").val()) !== undefined) {
+        var $ht = $("#id_html_montant_HT");
+        var $tva = $("#id_html_montant_TVA");
+        var $ttc = $("#id_html_montant_TTC");
+
+    } else if (($("#id_html_montant_avoirs_HT").val()) !== undefined) {
+        var $ht = $("#id_html_montant_avoirs_HT");
+        var $tva = $("#id_html_montant_avoirs_TVA");
+        var $ttc = $("#id_html_montant_avoirs_TTC");
+    }
+    var calcul = new Calcule($ht, $tva, $ttc);
+
 //// factures avoirs commandes devis ....
-
-  if (($("#id_html_montant_HT").val()) !== undefined) {
-    var $ht = $("#id_html_montant_HT");
-    var $tva = $("#id_html_montant_TVA");
-    var $ttc = $("#id_html_montant_TTC");
-
-  } else if (($("#id_html_montant_avoirs_HT").val()) !== undefined) {
-    var $ht = $("#id_html_montant_avoirs_HT");
-    var $tva = $("#id_html_montant_avoirs_TVA");
-    var $ttc = $("#id_html_montant_avoirs_TTC");
-  }
-
-  $ht.keyup(function () {
-    var valueHT = ($(this).val()) * 1;
-    var valueTVA = (valueHT * 0.2);
-
-    var valueTTC = plus(valueTVA, valueHT);
-    $tva.val(valueTVA.toFixed(2));
-    $ttc.val(valueTTC);
-
-
-  }).keyup();
-  $tva.keyup(function () {
-    var valueHT = ($ht.val()) * 1;
-    var valueTVA = ($(this).val()) * 1;
-
-    var valueTTC = plus(valueTVA, valueHT);
-
-
-    $ttc.val(valueTTC);
-  }).keyup();
-  $ttc.keyup(function () {
-    var valueTTC = ($(this).val()) * 1;
-    var valueHT = (valueTTC / 1.2);
-    var valueTVA = moins(valueTTC, valueHT);
-    $tva.val(valueTVA);
-    $ht.val(valueHT.toFixed(2));
-  }).keyup();
-
-
-
-
-
-
-
+    $ht.keyup(function() {
+        calcul.ht(this);
+    }).keyup();
+    $tva.keyup(function() {
+        calcul.tva(this);
+    }).keyup();
+    $ttc.keyup(function() {
+        calcul.ttc(this);
+    }).keyup();
 
 });
