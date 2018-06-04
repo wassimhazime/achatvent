c23ffe4d62596739c4c267468748abb4e5635173
wassim hazime <wassim@localhost.localdomain>
2018-06-04 14:55:35 +0000

    add modul update file

--- public/AWA/set_data_par_ajax.js
+++ public/AWA/set_data_par_ajax.js
@@ -1,17 +1,16 @@
 "use strict";
 (function() {
-    function _(el) {
-        return document.getElementById(el);
-    }
+
     $("form").submit(function(e) {
         e.preventDefault();
         var formdata = chargeFormData();
-        $("#progresshidden").removeClass("hidden")
-        $("form").addClass("hidden")
+        //$("#progresshidden").removeClass("hidden")
+       // $("form").addClass("hidden")
         //send formdata par ajax
+        $('#ModalProgress').modal('show');
         ajaxSendData(formdata);
     });
-    function gestion_errer(elment) {
+    function gestion_erreur(elment) {
         var not_hidden = elment.type != "hidden";
         var not_null = $(elment).data("set_null") == "NO";
         var is_null = $(elment).val() == "";
@@ -22,9 +21,9 @@
                 closeIcon: true,
                 type: 'red',
                 title: 'message!',
-                content: "SVP veuillez remplir le champ <strong>" + elment.name + " </strong>",
+                content: "SVP veuillez remplir le champ ==> <strong>" + elment.name + " </strong>",
             });
-            throw "erreur";
+            throw "erreur input vide import";
         }
         return true;
     }
@@ -35,11 +34,11 @@
         // new form ajax
         var formdata = new FormData();
         $("input:not([type='submit'],[type='reset'],[type='button'],[type='file']) ,textarea,select:not([multiple])").each(function(  ) {
-            gestion_errer(this);
+            gestion_erreur(this);
             formdata.append(this.name, $(this).val());
         });
         $("select[multiple]").each(function() {
-            gestion_errer(this);
+            gestion_erreur(this);
             var name = this.name;
             $($(this).val()).each(function() {
                 formdata.append(name, this);
@@ -47,7 +46,7 @@
 
         });
         $("input[type='file']").each(function() {
-            gestion_errer(this);
+            gestion_erreur(this);
             var name = this.name;
             $(this.files).each(function() {
                 formdata.append(name, this);
@@ -75,26 +74,26 @@
         return Math.round(number * factor) / factor;
     }
     function progressHandler(event) {
-
         var percent = (event.loaded / event.total) * 100;
-        _("progressBar").style.width = Math.round(percent) + "%";
-        _("etat").innerHTML = "<h3>" + Math.round(percent) + "%</h3>";
+        $("#progressBar").css("width", Math.round(percent) + "%");
+        $("#etat").html("<h3>" + Math.round(percent) + "%</h3>");
         var load = precisionRound(event.loaded * 0.000001, 2);
         var total = precisionRound(event.total * 0.000001, 2);
-        _("messageprogressBar").innerHTML = "Uploaded " + load + " MB of " + total;
+        $("#messageprogressBar").html("envoyer " + load + " MB en " + total +" MB");
     }
     function completeHandler(event) {
-        _("status").innerHTML = event.target.responseText;
-
-    }
+        $('#ModalProgress').modal('hide');
+        $("#status").html(event.target.responseText);
+ }
     function errorHandler(event) {
 
-        _("messageprogressBar").innerHTML = "Upload Failed";
+        $("#messageprogressBar").html("Upload Failed");
 
     }
     function abortHandler(event) {
-        _("messageprogressBar").innerHTML = "Upload Aborted";
+        $("#messageprogressBar").html("Upload Aborted");
     }
+    
 }
 
 
