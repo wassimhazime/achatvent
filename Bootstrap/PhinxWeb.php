34f78007a1d705af02342db2402caa86b87ea61c
wassim <wassim@localhost>
2018-10-27 12:10:13 +0100

    phinix

--- Bootstrap/PhinxWeb.php
+++ Bootstrap/PhinxWeb.php
@@ -18,17 +18,12 @@ if (true) {
 // - (string) "t" target version
 // - (boolean) "debug" enable debugging?
 // Get the phinx console application and inject it into TextWrapper.
-
-
-    $appphinix = new Phinx\Console\PhinxApplication();
-
-    $wrap = new Phinx\Wrapper\TextWrapper($appphinix);
-
 // Mapping of route names to commands.
     $routes = [
-        'status' => 'getStatus',
-        'migrate' => 'getMigrate',
-        'rollback' => 'getRollback',
+        'status' => 'getStatus', /// => /status
+        'migrate' => 'getMigrate',  /// => /migrate
+        'rollback' => 'getRollback',  /// => /rollback  or /rollback?t=0
+        'seed' => 'getSeed', /// => /seed
     ];
 
 // Extract the requested command from the URL, default to "status".
@@ -38,29 +33,27 @@ if (true) {
 
 // Verify that the command exists, or list available commands.
     if (isset($routes[$command])) {
+        $appphinix = new Phinx\Console\PhinxApplication();
+        $wrap = new Phinx\Wrapper\TextWrapper($appphinix);
         $commands = implode(', ', array_keys($routes));
-        //    header('Content-Type: text/plain', true, 404);
-        //  die("Command not found! Valid commands are: {$commands}.");
-// Get the environment and target version parameters.
+
+        // Get the environment and target version parameters.
         $env = isset($_GET['e']) ? $_GET['e'] : null;
         $target = isset($_GET['t']) ? $_GET['t'] : null;
+        // Execute the command and determine if it was successful.
+        $output = call_user_func([$wrap, $routes[$command]], $env, $target);
+
 
 // Check if debugging is enabled.
         $debug = !empty($_GET['debug']) && filter_var($_GET['debug'], FILTER_VALIDATE_BOOLEAN);
-
-
-
-
-// Execute the command and determine if it was successful.
-        $output = call_user_func([$wrap, $routes[$command]], $env, $target);
-        $error = $wrap->getExitCode() > 0;
-// Finally, display the output of the command.
-        header('Content-Type: text/plain', true, $error ? 500 : 200);
         if ($debug) {
-            // Show what command was executed based on request parameters.
+// Show what command was executed based on request parameters.
             $args = implode(', ', [var_export($env, true), var_export($target, true)]);
             echo "DEBUG: $command($args)" . PHP_EOL . PHP_EOL;
         }
+        $error = $wrap->getExitCode() > 0;
+        // Finally, display the output of the command.
+        header('Content-Type: text/plain', true, $error ? 500 : 200);
         echo $output;
         die();
     }
